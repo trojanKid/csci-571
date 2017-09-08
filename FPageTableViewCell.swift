9ebe2c6831b2dfd4798ac3c1823726e49234454b
trojanKid <31758266+trojanKid@users.noreply.github.com>
2017-09-08 11:54:23 -0700

    Add files via upload

---
+++ FPageTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  FPageTableViewCell.swift
+//  
+//
+//  Created by Shiji Zhou on 4/26/17.
+//
+//
+
+import UIKit
+
+class FPageTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ Podfile
@@ -0,0 +1,10 @@
+source 'https://github.com/CocoaPods/Specs.git'
+platform :ios, '10.0'
+use_frameworks!
+
+target 'hw9' do
+pod 'Alamofire', '~> 4.4.0'
+pod 'SwiftyJSON','~> 3.1.4'
+pod 'SwiftSpinner','~> 1.2.1'
+pod 'EasyToast','~> 2.0.2'
+end
